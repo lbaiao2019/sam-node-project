7555505a753906f5dc4fa1354b84a8d71a582511
Leonardo Baiao <lbaiao@morneaushepell.com>
2021-08-21 22:29:14 +0100

    fix api gateway

--- iac/api.tf
+++ iac/api.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_integration" "default" {
   rest_api_id = aws_api_gateway_rest_api.default.id
   resource_id = aws_api_gateway_resource.default.id
   http_method = aws_api_gateway_method.default.http_method
-  type        = "AWS"
+  type        = "AWS_PROXY"
   integration_http_method = var.http_method
   uri         = aws_lambda_function.default.arn
 }

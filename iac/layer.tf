596012ea87d9f7723fd03c7263324ff9bee826ea
Leonardo Baiao <lbaiao@morneaushepell.com>
2021-08-21 20:19:36 +0100

    test

--- iac/layer.tf
+++ iac/layer.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_layer_version" "layer" {
   layer_name          = local.service_name
   compatible_runtimes = [var.runtime]
-  filename            = local.filename
+  filename            = filebase64sha256(local.filename)
   source_code_hash    = filebase64sha256(local.filename)
 }
